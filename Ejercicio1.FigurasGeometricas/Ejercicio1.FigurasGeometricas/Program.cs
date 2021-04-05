73891dc2d93ad723b0124befa324fb040b66fc75
new <new@DESKTOP-KEGFG4B>
2021-04-05 00:41:04 -0300

    inicial commit

---
+++ Ejercicio1.FigurasGeometricas/Ejercicio1.FigurasGeometricas/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PCYP.MaidanaEduardo._2021
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int b, a, area, num;
+            string si;
+            do
+            {
+                Console.WriteLine("elija una figura geometrica");
+                Console.WriteLine("1.cuadrado");
+                Console.WriteLine("2.rectangulo");
+                Console.WriteLine("3.triangulo");
+                num = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine("ponga la base o el lado");
+                b = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine("ponga altura o el lado");
+                a = Convert.ToInt32(Console.ReadLine());
+
+                switch (num)
+                {
+                    case 1:
+                        area = b * a;
+                        Console.WriteLine("el area del cuadrado es : " + area);
+                        break;
+
+                    case 2:
+                        area = b * a;
+                        Console.WriteLine("el area del rectangulo es : " + area);
+                        break;
+
+                    case 3:
+                        area = b * a / 2;
+                        Console.WriteLine("el area del triangulo es : " + area);
+
+                        break;
+                }
+            }
+
+
+
+            Console.WriteLine("Desea Volver a intentarlo");
+            si = Console.ReadLine();
+
+        }
+    }
+}
